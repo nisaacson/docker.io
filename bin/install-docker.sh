39d5c499e00cb88e037159ce5b08d173f8fa6f1a
Noah Isaacson <noah@nisaacson.com>
2013-07-22 13:30:36 -0600

    Use linux-image-extra-virtual package

--- bin/install-docker.sh
+++ bin/install-docker.sh
@@ -1,5 +1,5 @@
 sudo apt-get install -qq software-properties-common
 sudo add-apt-repository -y ppa:dotcloud/lxc-docker
 sudo apt-get update
-sudo apt-get install -qq linux-image-extra-`uname -r`
+sudo apt-get install -qq linux-image-extra-virtual
 sudo apt-get install -qq lxc-docker
