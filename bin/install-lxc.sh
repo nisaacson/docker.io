6c966189a1aff502f3f2d0313dc99a211801543e
Noah Isaacson <noah@nisaacson.com>
2013-07-22 10:44:59 -0600

    Add missing sudo prefix to install-lxc.sh script

--- bin/install-lxc.sh
+++ bin/install-lxc.sh
@@ -18,11 +18,11 @@
 #
 ################################################################################
 #!/bin/bash
-apt-get -qq update
+sudo apt-get -qq update
 cat <<EOF | tee /usr/sbin/policy-rc.d
 #!/bin/sh
 exit 101
 EOF
-chmod 755 /usr/sbin/policy-rc.d
-apt-get -qq install lxc
+sudo -qq chmod 755 /usr/sbin/policy-rc.d
+sudo apt-get -qq install lxc
 lxc-version
